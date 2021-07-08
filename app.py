5970a2b556172c54fe3c9aa12aebec7bcc9d65f9
astudillome <projectagility@gmail.com>
2021-07-08 14:11:21 -0600

    added dependencies

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_migrate import Migrate
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+db = SQLAlchemy()
 
-APP = create_app()
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/moviedata'
+app.config['SECRET_KEY'] = os.urandom(32)
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
